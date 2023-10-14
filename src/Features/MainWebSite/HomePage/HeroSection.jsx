62b0a4543892deb0a01bb70359dff0270997ebd7
abusaidshabib <abusaidshabib712@gmail.com>
2023-10-14 08:54:07 +0600

    herosection done

--- src/Features/MainWebSite/HomePage/HeroSection.jsx
+++ src/Features/MainWebSite/HomePage/HeroSection.jsx
@@ -1,9 +1,13 @@
+import dark1 from "../../../assets/BackgroundDark1.svg";
+import dark2 from "../../../assets/BackgroundOrange1.svg";
+import mainBack from "../../../assets/jobBackground.jpg";
+
 const HeroSection = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="min-h-screen min-w-screen relative">
+
+    </div>
+  );
 };
 
 export default HeroSection;
